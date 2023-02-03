25e2175a0f4b050f44e33edb860ed242b213a488
BukanKenjan <118797509+bukankenjan@users.noreply.github.com>
2023-02-03 22:05:59 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
-BOT_NAME = os.environ.get("BOT_NAME", "")
-CSE_KEY = os.environ.get("CSE_KEY", "")
-CSE_CX = os.environ.get("CSE_CX", "")
-API_TOKEN = os.environ.get("API_TOKEN", "")
-WEBHOOK_URL = os.environ.get("WEBHOOK_URL", "")
+BOT_NAME = os.environ.get("⊕ Judi Teros", "")
+CSE_KEY = os.environ.get("AIzaSyAM9xdJ3KMKqTrFEHpjcGnnWub52I4akwg", "")
+CSE_CX = os.environ.get("10eb55a5c7640430a", "")
+API_TOKEN = os.environ.get("5698296189:AAEiiQo3OlZWqYvPL9WBACqdI8VYVLBIgwQ", "")
+WEBHOOK_URL = os.environ.get("https://google.com/", "")
 NGINX_SUBPATH = os.environ.get("NGINX_SUBPATH", "")
 BATCH = int(os.environ.get("BATCH", 10))
 POLLING = bool(os.environ.get("POLLING", False))
